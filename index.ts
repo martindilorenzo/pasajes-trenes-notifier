5d5e9a0ce7523e3e08d99f96f37b486213686bcc
martindilorenzo <dimartechnologies@gmail.com>
2022-09-16 14:34:24 -0300

    cuando hay pasajes, solo envia 3 mails

--- index.ts
+++ index.ts
@@ -17,7 +17,10 @@ const FREQUENCY = '*/10 * * * * *';
 
 const MENSAJE = "La venta de pasajes a Mar del Plata, Pinamar, Bahía Blanca, Rufino / Justo Daract, Bragado / Pehuajó, Junín, Tucumán, Córdoba y Rosario se encuentra habilitada hasta el 31/10/22 inclusive";
 
+let cantAvisos = 0;
+
 cron.schedule(FREQUENCY, async () => {
+  
   console.log(`Running on: ${new Date().toLocaleString('es-AR', { timeZone: 'America/Buenos_Aires' })}`)
 
   const browser = await chromium.launch()
@@ -25,16 +28,19 @@ cron.schedule(FREQUENCY, async () => {
   await page.goto('https://www.argentina.gob.ar/transporte/trenes-argentinos/pasajes-larga-distancia')
 
   const content = await page.innerHTML('div[class="media-body"] strong')
-  //este if va con === Se cambio para probar si funciona sendMail
-  if (content !== MENSAJE) {
+
+  if (content === MENSAJE) {
     console.log("NO HAY PASAJES NUEVOS");
   } else {
-    console.log("entra al IF");
     notifier.notify({
       title: "PASAJES NUEVOS",
       message: "HAY PASAJES NUEVOS!!!!!!!!!!"
     })
-    sendEmail();    
+    console.log(cantAvisos);
+    if (cantAvisos < 3) {
+      sendEmail(); 
+      cantAvisos++;
+    }
   }
   
   await browser.close()
--- sendEmail.ts
+++ sendEmail.ts
@@ -5,15 +5,15 @@ export const sendEmail: () => void = () => {
     service: "gmail",
     auth: {
       user: "martin.mdp92@gmail.com",
-      pass: "cbjjxdbkhvrjoatc",
+      pass: "AGREGAR_PASS",
     },
   });
 
   const mailOptions = {
     from: "martin.mdp92@gmail.com",
     to: "martinrdilorenzo@gmail.com",
-    subject: "HAY PASAJES ",
-    text: "HAY PASAJES DE TREN. ENTRA A LA WEB DE TRENES ARGENTINOS",
+    subject: "HAY PASAJES DE TREN",
+    text: "HAY PASAJES DE TREN. ENTRA A LA WEB DE TRENES ARGENTINOS: https://www.argentina.gob.ar/transporte/trenes-argentinos/pasajes-larga-distancia",
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
